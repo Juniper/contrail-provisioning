aa0c55cf1bdc6594f4a87cf3f8e8fe293335ba90
Atul Moghe <amoghe@juniper.net>
2014-02-11 11:57:55 -0800

    setup scripts modification to bringup havana based ubuntu packages

--- contrail_setup_utils/quantum-server-setup.sh
+++ contrail_setup_utils/quantum-server-setup.sh
@@ -18,24 +18,40 @@
 CONF_DIR=/etc/contrail
 set -x
 
+if [ -f /etc/redhat-release ]; then
+   is_redhat=1
+   is_ubuntu=0
+   web_svc=httpd
+   mysql_svc=mysqld
+   msg_svc=qpidd
+fi
+
+if [ -f /etc/lsb-release ]; then
+   is_ubuntu=1
+   is_redhat=0
+   web_svc=apache2
+   mysql_svc=mysql
+   msg_svc=rabbitmq-server
+fi
+
 function error_exit
 {
     echo "${PROGNAME}: ${1:-''} ${2:-'Unknown Error'}" 1>&2
     exit ${3:-1}
 }
 
-chkconfig mysqld 2>/dev/null
+chkconfig $mysql_svc 2>/dev/null
 ret=$?
 if [ $ret -ne 0 ]; then
     echo "MySQL is not enabled, enabling ..."
-    chkconfig mysqld on 2>/dev/null
+    chkconfig $mysql_svc on 2>/dev/null
 fi
 
-service mysqld status 2>/dev/null
+service $mysql_svc status 2>/dev/null
 ret=$?
 if [ $ret -ne 0 ]; then
     echo "MySQL is not active, starting ..."
-    service mysqld restart 2>/dev/null
+    service $mysql_svc restart 2>/dev/null
 fi
 
 # Use MYSQL_ROOT_PW from the environment or generate a new password
@@ -72,7 +88,12 @@ export OS_NO_CACHE=1
 EOF
 
 # Update all config files with service username and password
-for svc in quantum; do
+if [ -d /etc/neutron ]; then
+    net_svc_name='neutron'
+else
+    net_svc_name='quantum'
+fi
+for svc in $net_svc_name; do
     openstack-config --set /etc/$svc/$svc.conf DEFAULT bind_port $QUANTUM_PORT
     openstack-config --set /etc/$svc/$svc.conf keystone_authtoken admin_tenant_name service
     openstack-config --set /etc/$svc/$svc.conf keystone_authtoken admin_user $svc
@@ -84,24 +105,34 @@ openstack-config --set /etc/quantum/quantum.conf QUOTAS quota_network -1
 openstack-config --set /etc/quantum/quantum.conf QUOTAS quota_subnet -1
 openstack-config --set /etc/quantum/quantum.conf QUOTAS quota_port -1
 
-openstack-config --set /etc/quantum/quantum.conf DEFAULT core_plugin quantum.plugins.contrail.ContrailPlugin.ContrailPlugin
-openstack-config --set /etc/quantum/quantum.conf DEFAULT log_format '%(asctime)s.%(msecs)d %(levelname)8s [%(name)s] %(message)s'
+if [ -d /etc/neutron ]; then
+    openstack-config --set /etc/neutron/neutron.conf DEFAULT core_plugin neutron.plugins.juniper.contrail.contrailplugin.ContrailPlugin
+    if [ $is_redhat -eq 1 ] ; then
+        openstack-config --set /etc/neutron/neutron.conf DEFAULT qpid_hostname $CONTROLLER
+    else
+        openstack-config --set /etc/neutron/neutron.conf DEFAULT rabbit_host $CONTROLLER
+    fi
+else
+    openstack-config --set /etc/quantum/quantum.conf DEFAULT core_plugin quantum.plugins.contrail.ContrailPlugin.ContrailPlugin
+fi
+
+openstack-config --set /etc/$net_svc_name/$net_svc_name.conf DEFAULT log_format '%(asctime)s.%(msecs)d %(levelname)8s [%(name)s] %(message)s'
 
 echo "======= Enabling the services ======"
 
-for svc in qpidd httpd memcached; do
+for svc in $msg_svc $web_svc memcached; do
     chkconfig $svc on
 done
 
-for svc in quantum-server; do
+for svc in $net_svc_name-server; do
     chkconfig $svc on
 done
 
 echo "======= Starting the services ======"
 
-for svc in qpidd httpd memcached; do
+for svc in $msg_svc $web_svc memcached; do
     service $svc restart
 done
 
-service quantum-server restart
+service $net_svc_name-server restart
 
