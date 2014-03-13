6c3b9093397c6928e0cb81b4a1d9226fa3f5af88
Ananth Suryanarayana <Anantha@juniper.net>
2014-03-13 13:52:37 -0700

    Configuration format changes for collector and query-engine
    They now take configuration in the standard ini format.
    Default configuration file is read off /etc/contrail/collector.conf and
    /etc/contrail/query-engine.conf
    
    One can override the values from the config file through command line option
    as well. e.g. --DEFAULT.log_level=DEBUG
    
    Use --help to see various options available. Package build also places the
    default config files under /etc/contrail/.
    
    When changes are made to the configuration file, the process must be _restarted_
    to take effect. (e.g. service supervisord-analytics restart).

--- templates/qe_param_template.py
+++ templates/qe_param_template.py
@@ -1,12 +1,33 @@
 import string
 
-template = string.Template("""
-CASSANDRA_SERVER_LIST=$__contrail_cassandra_server_list__
-REDIS_SERVER=$__contrail_redis_server__
-REDIS_SERVER_PORT=$__contrail_redis_server_port__
-HTTP_SERVER_PORT=$__contrail_http_server_port__
-LOG_FILE=$__contrail_log_file__
-LOG_LOCAL=$__contrail_log_local__
-COLLECTOR=$__contrail_collector__
-COLLECTOR_PORT=$__contrail_collector_port__
+template = string.Template("""#
+# Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
+#
+# Query-Engine configuration options
+#
+
+[DEFAULT]
+# analytics_data_ttl=
+  cassandra_server_list=$__contrail_cassandra_server_list__
+  collectors=$__contrail_collector__:$__contrail_collector_port__
+  hostip=$__contrail_host_ip__ # Resolved IP of `hostname`
+# hostname= # Retrieved as `hostname`
+  http_server_port=$__contrail_http_server_port__
+# log_category=
+# log_disable=0
+  log_file=$__contrail_log_file__
+# log_files_count=10
+# log_file_size=1048576 # 1MB
+# log_level=SYS_NOTICE
+# log_local=0
+# test_mode=0
+
+[DISCOVERY]
+# port=5998
+  server=$__contrail_discovery_ip__ # discovery_server IP address
+
+[REDIS]
+  port=$__contrail_redis_server_port__
+  server=$__contrail_redis_server__
+
 """)
