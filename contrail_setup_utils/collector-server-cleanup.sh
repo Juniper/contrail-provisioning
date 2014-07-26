4a6b59777c3b6aba9a4cabd52520f97966d8308f
Chandan Mishra <cdmishra@juniper.net>
2014-07-25 17:53:04 -0700

    Bug fix for https://bugs.launchpad.net/juniperopenstack/+bug/1344477
    Change-Id: I707af99316c7b2c5e8edbd2103c639f313590a8c

--- contrail_setup_utils/collector-server-cleanup.sh
+++ contrail_setup_utils/collector-server-cleanup.sh
@@ -2,7 +2,7 @@
 
 #cleanup script for analytics package under supervisord
 
-for svc in contrail-collector contrail-opserver; do
+for svc in contrail-collector contrail-analytics-api ; do
     supervisorctl -s http://localhost:9002 stop $svc
 done
 chkconfig supervisor-analytics off
