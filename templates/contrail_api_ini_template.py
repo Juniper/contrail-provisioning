69123820b278d47121e07cddd1a8c038585a5edb
Chandan Mishra <cdmishra@juniper.net>
2014-07-25 17:53:04 -0700

    Bug fix for https://bugs.launchpad.net/juniperopenstack/+bug/1344477
    Change-Id: I707af99316c7b2c5e8edbd2103c639f313590a8c

--- templates/contrail_api_ini_template.py
+++ templates/contrail_api_ini_template.py
@@ -2,7 +2,7 @@ import string
 
 template = string.Template("""
 [program:contrail-api]
-command=/usr/bin/contrail-api --conf_file /etc/contrail/api_server.conf --listen_port $__contrail_api_port_base__%(process_num)01d --worker_id %(process_num)s
+command=/usr/bin/contrail-api --conf_file /etc/contrail/contrail-api.conf --listen_port $__contrail_api_port_base__%(process_num)01d --worker_id %(process_num)s
 numprocs=$__contrail_api_nworkers__
 process_name=%(process_num)s
 redirect_stderr=true
