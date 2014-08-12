a2be0fdbc06e07d3b44539b43af3298ca350adf9
arvindvis <arvindv@juniper.net>
2014-08-12 15:51:50 -0700

    Changes are applicable for both ubuntu and centos
    Change-Id: I4b89ce680c36239ab15c9ff75896cda6fdd117f5

--- contrail_setup_utils/setup.py
+++ contrail_setup_utils/setup.py
@@ -1114,7 +1114,7 @@ HWADDR=%s
                 rabbit_host = self._args.internal_vip
                 rabbit_port = 5673
 
-            if pdist == 'Ubuntu':
+            if pdist == 'Ubuntu' or pdist == 'centos':
                 # log4j.properties
                 template_vals = {
                                 }
