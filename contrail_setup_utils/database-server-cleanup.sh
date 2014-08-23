fada24c264d2d042adfb0d3a53905f3efac55071
Chandan Mishra <cdmishra@juniper.net>
2014-08-22 17:14:35 -0700

    bug fix for bugs 1355006, 1354579, 1354581, 1354579
    Change-Id: I1eddcfbf7b1d7f16b930ab30ff4cf7525ff388ba

--- contrail_setup_utils/database-server-cleanup.sh
+++ contrail_setup_utils/database-server-cleanup.sh
@@ -2,7 +2,7 @@
 
 #cleanup script for database package under supervisord
 # shutdown all the services
-for svc in zookeeper supervisord-contrail-database; do
+for svc in zookeeper supervisor-database; do
     chkconfig $svc off > /dev/null 2>&1
     service $svc stop > /dev/null 2>&1
 done
