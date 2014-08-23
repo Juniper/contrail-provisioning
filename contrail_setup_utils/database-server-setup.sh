ec5bf7fa58dab9278f362330af58fe4a56d59b60
Chandan Mishra <cdmishra@juniper.net>
2014-08-22 17:14:35 -0700

    bug fix for bugs 1355006, 1354579, 1354581, 1355712, 1354579
    Change-Id: I1eddcfbf7b1d7f16b930ab30ff4cf7525ff388ba

--- contrail_setup_utils/database-server-setup.sh
+++ contrail_setup_utils/database-server-setup.sh
@@ -3,13 +3,13 @@
 #setup script for analytics package under supervisord
 echo "======= Enabling the services ======"
 
-for svc in zookeeper supervisord-contrail-database; do
+for svc in zookeeper supervisor-database; do
     chkconfig $svc on
 done
 
 echo "======= Starting the services ======"
 
-for svc in zookeeper supervisord-contrail-database; do
+for svc in zookeeper supervisor-database; do
     service $svc restart
 done
 
