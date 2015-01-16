a1fed12055371830853ce57d859dffa150c2296c
Sunil Basker <sunilbasker@juniper.net>
2015-01-16 20:43:25 +0530

    Related-Bug: #1404940
    ->  sets MTU for vhost0 intf to 1496, for esxi compute-vm
    ->  sets MTU for eth1 intf to vmpg switch's MTU, for esxi compute-vm
    
    Change-Id: I6c03ce76a4a78ed930f8abb329016a0cb8873df5

--- contrail_provisioning/compute/common.py
+++ contrail_provisioning/compute/common.py
@@ -269,7 +269,8 @@ SUBCHANNELS=1,2,3
         # end self.pdist == centos | fedora | redhat
 
         if self.pdist in ['Ubuntu']:
-            self._rewrite_net_interfaces_file(self.dev, self.mac, self.vhost_ip, self.netmask, self.gateway)
+            self._rewrite_net_interfaces_file(self.dev, self.mac, self.vhost_ip, self.netmask, self.gateway,
+                    self._args.vmware, self._args.vmware_vmpg_vswitch_mtu)
         # end self.pdist == ubuntu
 
         else: # of if self.dev and self.dev != 'vhost0'
--- contrail_provisioning/compute/setup.py
+++ contrail_provisioning/compute/setup.py
@@ -47,6 +47,7 @@ class ComputeSetup(ContrailSetup):
             'vmware_username': 'root',
             'vmware_passwd': 'c0ntrail123',
             'vmware_vmpg_vswitch': 'c0ntrail123',
+            'vmware_vmpg_vswitch_mtu': '9000',
             'no_contrail_openstack': False,
             'no_nova_config': False,
             'orchestrator': 'openstack',
@@ -95,6 +96,7 @@ class ComputeSetup(ContrailSetup):
         parser.add_argument("--vmware_username", help = "The Vmware ESXI username")
         parser.add_argument("--vmware_passwd", help = "The Vmware ESXI password")
         parser.add_argument("--vmware_vmpg_vswitch", help = "The Vmware VMPG vswitch name")
+        parser.add_argument("--vmware_vmpg_vswitch_mtu", help = "The Vmware VMPG vswitch MTU")
         parser.add_argument("--internal_vip", help = "Internal VIP Address of openstack nodes")
         parser.add_argument("--external_vip", help = "External VIP Address of openstack nodes")
         parser.add_argument("--contrail_internal_vip", help = "VIP Address of config  nodes")
