6aee000bb8c2e95d911dadd7ca2e83f9cd1053b3
Ranjeet R <rranjeet@juniper.net>
2015-03-11 16:23:06 -0700

    Fixes: Openstack HA , mysql gets locked up during node failure test.
    We have a cleanup script which cleans up CMON and Keystone tokens
    every night. CMON produces a lot of logs (recorded into the database)
    and hence the CMON DB in MySQL is pretty huge. When the CMON purge
    script runs it results in data inconsistency between MySQL nodes
    which is known to happen for DELETE operations in large MySQL tables.
    
    The following change prevents this by doing the following -
    * The time interval which CMON logs data is increased from 30 seconds
    to 24 hours.
    * Running CMON purge operation only in a node where CMON is running.
    * Running cleanup every hour
    * Truncating tables instead of deleting the rows based on condition
      which might take longer time. The delete commands are running
      after the truncate which would happen quickly.
    
    Change-Id: Id6df179bacdfeb2e9ac05fd0c58bb8edc1253b86
    Closes: 1414245

--- contrail_provisioning/openstack/scripts/contrail-token-clean.sh
+++ contrail_provisioning/openstack/scripts/contrail-token-clean.sh
@@ -4,10 +4,15 @@
 # validated the request.
 # Author - Sanju Abraham
 
+source /etc/contrail/ha/cmon_param
+
 LOGFILE=/var/log/contrail/ha/token-cleanup.log
+MYIPS=$(ip addr show | sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')
+viponme=0
 mysql_user=keystone
 mysql_password=keystone
-mysql_host=localhost
+mysql_host=$VIP
+mysql_port=33306
 mysql=$(which mysql)
 cmon_user_pass=cmon
 cmon_stats_purge="call sp_cmon_purge_history;"
@@ -31,16 +36,53 @@ log_info_msg() {
     echo "$(timestamp): INFO: $msg" >> $LOGFILE
 }
 
-log_info_msg "keystone-cleaner::Starting Keystone 'token' table cleanup"
+for myip in $MYIPS
+ do
+  if [ $myip == $VIP ]; then
+     viponme=1
+     break
+  fi
+ done
 
-log_info_msg "keystone-cleaner::Starting token cleanup"
-mysql -u${mysql_user} -p${mysql_password} -h${mysql_host} -e 'USE keystone ; DELETE FROM token WHERE NOT DATE_SUB(CURDATE(),INTERVAL 1 DAY) <= expires;'
-valid_token=$($mysql -u${mysql_user} -p${mysql_password} -h${mysql_host} -e 'USE keystone ; SELECT * FROM token;' | wc -l)
-log_info_msg "keystone-cleaner::Finishing token cleanup, there is still $valid_token valid tokens..."
+if [ $viponme -eq 1 ]; then
+    log_info_msg "keystone-cleaner::Starting token cleanup"
+
+    mysql -u${mysql_user} -p${mysql_password} -h${mysql_host} -P${mysql_port} -e "USE keystone ; DELETE FROM token where expires <= convert_tz(now(),@@session.time_zone,'+01:00');"
+    valid_token=$($mysql -u${mysql_user} -p${mysql_password} -h${mysql_host} -P${mysql_port} -e "USE keystone ; SELECT count(*) FROM token;")
+    valid_token=$(echo $valid_token | awk '{print $2}')
+
+    log_info_msg "keystone-cleaner::Finishing token cleanup, there are $valid_token valid tokens..."
+
+    mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -P${mysql_port} -e "use cmon; truncate table cmon_log;"
+    mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -P${mysql_port} -e "use cmon; truncate table memory_usage_history;"
+    mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -P${mysql_port} -e "use cmon; truncate table cpu_stats_history;"
+    mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -P${mysql_port} -e "use cmon; truncate table ram_stats_history;"
+    mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -P${mysql_port} -e "use cmon; truncate table disk_stats_history;"
+    mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -P${mysql_port} -e "use cmon; truncate table diskdata_history;"
+    mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -P${mysql_port} -e "use cmon; truncate table mysql_global_statistics_history;"
+    mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -P${mysql_port} -e "use cmon; truncate table mysql_statistics_history;"
+    mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -P${mysql_port} -e "use cmon; truncate table mysql_statistics_tm;"
+    mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -P${mysql_port} -e "use cmon; truncate table mysql_advisor_history;"
+    mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -P${mysql_port} -e "use cmon; truncate table expression_result_history;"
+    mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -P${mysql_port} -e "use cmon; truncate table mysql_performance_results;"
+    mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -P${mysql_port} -e "use cmon; truncate table net_stats_history;"
+    mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -P${mysql_port} -e "use cmon; truncate table cmon_job;"
+    mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -P${mysql_port} -e "use cmon; truncate table cmon_job_message;"
+    mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -P${mysql_port} -e "use cmon; truncate table cluster_log;"
+    mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -P${mysql_port} -e "use cmon; truncate table backup_log;"
+    mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -P${mysql_port} -e "use cmon; truncate table restore_log;"
+    mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -P${mysql_port} -e "use cmon; truncate table backup;"
+    mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -P${mysql_port} -e "use cmon; truncate table restore;"
+    mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -P${mysql_port} -e "use cmon; truncate table alarm_log;"
+    mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -P${mysql_port} -e "use cmon; truncate table mysql_query_histogram;"
+    mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -P${mysql_port} -e "use cmon; truncate table mysql_slow_queries;"
+    mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -P${mysql_port} -e "use cmon; ${cmon_stats_purge}"
+    log_info_msg "Purged cmon stats history"
+else
+    log_info_msg "VIP is not in this node and hence not running the Keystone and CMON DB purge"
+fi
 
 find /var/log/contrail/ha/ -size +10240k -exec rm -f {} \;
 find /var/log/cmon.log -size +10240k -exec rm -f {} \;
 
-mysql -u${cmon_user_pass} -p${cmon_user_pass} -h${mysql_host} -e "use cmon; ${cmon_stats_purge}"
-log_info_msg "Purged cmon stats history"
 exit 0
