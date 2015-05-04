39ee88192f52ec4aac6182e00431dd0dcd37d33f
Ranjeet R <rranjeet@juniper.net>
2015-05-04 14:04:17 -0700

    Fixes: This is an enhancement to separate cmon tables from galera cluster
    While upgrading from 2.0 or 2.1 to 2.2, we have to make sure that
    the cmon tables are removed from galera clustering.
    
    Hence, we are dropping the cmon database and reprovisioning cmon.
    
    Change-Id: I2455193cd2e16dc9ac342e17b4e1a56e01204b5f
    Close-Bug: #1443708

--- contrail_provisioning/openstack/upgrade.py
+++ contrail_provisioning/openstack/upgrade.py
@@ -31,7 +31,8 @@ class OpenstackUpgrade(ContrailUpgrade, OpenstackSetup):
         if self._args.internal_vip:
             backup_data += ['/etc/mysql',
                             '/etc/keepalived',
-                            '/etc/contrail/ha']
+                            '/etc/contrail/ha',
+                            '/etc/cmon.cnf']
         self.upgrade_data['backup'] += backup_data
 
         self.upgrade_data['restore'] = self.upgrade_data['backup']
@@ -45,10 +46,21 @@ class OpenstackUpgrade(ContrailUpgrade, OpenstackSetup):
     def restart(self):
         local('service supervisor-openstack restart')
 
+    def fix_cmon_config(self):
+        with settings(warn_only=True):
+            local('service contrail-hamon stop')
+            local('kill -9 $(pidof cmon)')
+            local("mysql -uroot -p$(cat /etc/contrail/mysql.token) -e 'drop database cmon'")
+            local("sed -i '/pidfile=\/var\/run\//c\pidfile=\/var\/run\/cmon\/' /etc/cmon.cnf")
+
     def upgrade(self):
         self.stop()
         self._upgrade()
         self.upgrade_python_pkgs()
+        if (self._args.internal_vip and
+            self._args.from_rel < 2.2 and
+            self._args.to_rel >= 2.2):
+            self.fix_cmon_config()
         self.restart()
 
 
