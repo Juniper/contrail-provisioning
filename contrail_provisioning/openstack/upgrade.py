7d9455c7b827a5a2a1ba8e8ae8a178aef755fbd3
Rudra Rugge <rrugge@juniper.net>
2015-05-11 10:00:38 -0700

    Add collector configuration in contrail plugin
    Add collector configuration in contrail plugin to retrieve
    loadbalancer stats
    
    Change-Id: I0a8498122e583cf6267270e6aeef34b2fbf4de46
    Partial-Bug: #1434617

--- contrail_provisioning/openstack/upgrade.py
+++ contrail_provisioning/openstack/upgrade.py
@@ -49,6 +49,15 @@ class OpenstackUpgrade(ContrailUpgrade, OpenstackSetup):
         self.stop()
         self._upgrade()
         self.upgrade_python_pkgs()
+
+        # Populate collector configuration to retrieve loadbalancer stats
+        if (self._args.from_rel < 2.2 and self._args.to_rel >= 2.2):
+            conf_file = '/etc/neutron/plugins/opencontrail/ContrailPlugin.ini'
+            local('openstack-config --set %s COLLECTOR analytics_api_ip %s' % \
+                (conf_file, self._args.internal_vip or self._args.self_ip))
+            local('openstack-config --set %s COLLECTOR analytics_api_port %s' % \
+                (conf_file, '8081'))
+
         self.restart()
 
 
