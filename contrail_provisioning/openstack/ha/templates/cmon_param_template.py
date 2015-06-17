f06d4e952a297e87aa0a0acffb819867eca74545
Sanju Abraham <asanju@gmail.com>
2015-06-16 23:44:06 -0700

    Close-Bug: #1465501. Fix disables rabbitMQ resets on failure detection                      Made changes as per the review comments to ensure dups are removed during upgrade                      and the params new or modified are labelled for better managebility
    Change-Id: I13d1a1f6cd9bfcbe2aa3331e16b62791f454dd14

--- contrail_provisioning/openstack/ha/templates/cmon_param_template.py
+++ contrail_provisioning/openstack/ha/templates/cmon_param_template.py
@@ -5,6 +5,6 @@ VIP="$__internal_vip__"
 DIPS=($__haproxy_dips__)
 DIPS_SIZE=${#DIPS[@]}
 EVIP="$__external_vip__"
-PERIODIC_RMQ_CHK_INTER=120
-RABBITMQ_RESET=True
+PERIODIC_RMQ_CHK_INTER=60
+RABBITMQ_RESET=False
 """)
