d88f8826d4572ccd422c217fa92afacadabbf4fe
Ignatious Johnson Christopher <ijohnson@juniper.net>
2015-08-25 10:57:23 -0700

    Removing admin_token from the contrail config, webui auth conf files. So that the username/password will be used for authentication. Partial-Bug: 1473177
    Change-Id: If019a033c1e285715c0fa3aaae8b68976244e1ac
    (cherry picked from commit 2517dcb54f2b47ab22241308cfb9e9c376a9f316)

--- contrail_provisioning/common/templates/contrail_keystone_auth_conf.py
+++ contrail_provisioning/common/templates/contrail_keystone_auth_conf.py
@@ -7,7 +7,6 @@ auth_protocol=$__contrail_ks_auth_protocol__
 auth_port=$__contrail_ks_auth_port__
 admin_user=$__contrail_admin_user__
 admin_password=$__contrail_admin_password__
-admin_token=$__contrail_admin_token__
 admin_tenant_name=$__contrail_admin_tenant_name__
 insecure=$__keystone_insecure_flag__
 $__contrail_memcached_opt__
