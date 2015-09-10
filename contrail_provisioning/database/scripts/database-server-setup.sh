0d11bde7db529357f4154216f29af6f3d68c8cc1
Megh Bhatt <meghb@juniper.net>
2015-09-09 18:23:22 -0700

    Start contrail-database as part of database-server-setup.sh so that verify database fabric task does not fail Partial-Bug: #1484297
    Change-Id: Id64440c199fc3d9135173199e4e26a6172323e8c
    (cherry picked from commit 8f463f0b444c765df284f329cd62cbab4ace6978)

--- contrail_provisioning/database/scripts/database-server-setup.sh
+++ contrail_provisioning/database/scripts/database-server-setup.sh
@@ -3,13 +3,13 @@
 #setup script for analytics package under supervisord
 echo "======= Enabling the services ======"
 
-for svc in zookeeper supervisor-database; do
+for svc in zookeeper supervisor-database contrail-database; do
     chkconfig $svc on
 done
 
 echo "======= Starting the services ======"
 
-for svc in zookeeper supervisor-database; do
+for svc in zookeeper supervisor-database contrail-database; do
     service $svc restart
 done
 
