896a6fc73a8ce97d8b2bd6fe1f8e12b25dfef67e
Sanju Abraham <asanju@gmail.com>
2015-10-28 18:43:26 -0700

    Closes-Bug: #1494892             Backporting the config required for SSL changes.             Commit in https://review.opencontrail.org/#/c/14438/ is being ported to 2.21.x
    Change-Id: Iec9672c45d05f58eb5a777f588cc8521ee77b34b

--- contrail_provisioning/config/templates/vnc_api_lib_ini.py
+++ contrail_provisioning/config/templates/vnc_api_lib_ini.py
@@ -9,6 +9,12 @@ WEB_SERVER = 127.0.0.1
 WEB_PORT = 8082 ; connection to api-server directly
 BASE_URL = /
 ;BASE_URL = /tenants/infra ; common-prefix for all URLs
+;insecure=False
+;certfile=$__api_server_cert_file__
+;keyfile=$__api_server_key_file__
+;cafile=$__api_server_ca_file__
+;MAX_POOLS=100
+;MAX_CONNS_PER_POOL=100
 
 ; Authentication settings (optional)
 [auth]
@@ -17,5 +23,9 @@ AUTHN_PROTOCOL = http
 AUTHN_SERVER=$__contrail_keystone_ip__
 AUTHN_PORT = 35357
 AUTHN_URL = /v2.0/tokens
+;insecure=False
+;certfile=$__keystone_cert_file__
+;keyfile=$__keystone_key_file__
+;cafile=$$__keystone_ca_file__
 """)
 
