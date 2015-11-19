322e93cddc5206a25b249972627d34eb70a6fdba
Ranjeet R <rranjeet@juniper.net>
2015-11-19 11:45:02 -0800

    Fixes:  Supports HA monitoring even in cases where keepalived is not configured to provide VIP
    Supports HA monitoring even in cases where
    keepalived is not configured to provide VIP
    
    Patch-1: Upgrade support added
    Patch-2: Added cmon conf changes
    Patch-3: Incorporated changes from review
    
    Change-Id: I7d1612e1bb0c38955d84dea54d726fa2e6519c5a
    Closes-Bug: #1471596.

--- contrail_provisioning/openstack/ha/templates/cmon_param_template.py
+++ contrail_provisioning/openstack/ha/templates/cmon_param_template.py
@@ -10,4 +10,9 @@ RABBITMQ_RESET=True
 RABBITMQ_MNESIA_CLEAN=False
 RMQ_CLIENTS=("nova-conductor" "nova-scheduler")
 ZK_SERVER_IP="$__zooipports__"
+OS_KS_USER="$__keystoneuser__"
+OS_KS_PASS="$__keystonepass__"
+CMON_USER="$__cmonuser__"
+CMON_PASS="$__cmonpass__"
+MONITOR_GALERA="$__monitorgalera__"
 """)
