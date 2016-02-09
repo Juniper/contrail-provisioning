cd29ca426aa8f3c56679b4fc743fb6bfe111e439
Ignatious Johnson Christopher <ijohnson@juniper.net>
2016-02-09 14:13:45 -0800

    SSL Support for keystone and api-server during provisioing
    Change-Id: I92892aa8f8e76b5ab0aabe92d1c2bcefaaef7625
    Partial-bug: 1547965

--- contrail_provisioning/openstack/ha/scripts/contrail-ha-keystone-setup.sh
+++ contrail_provisioning/openstack/ha/scripts/contrail-ha-keystone-setup.sh
@@ -33,6 +33,12 @@
 
 set -x
 
+if [ $AUTH_PROTOCOL == 'https' ]; then
+    export INSECURE_FLAG='--insecure'
+else
+    export INSECURE_FLAG=''
+fi  
+
 ENABLE_ENDPOINTS=yes
 #ENABLE_QUANTUM=yes
 if [ -f /etc/redhat-release ]; then
@@ -86,7 +92,7 @@ function get_id () {
 function is_keystone_up() {
     for i in {1..36} {
     do
-       keystone tenant-list
+       keystone $INSECURE_FLAG tenant-list
        if [ $? == 0 ]; then
            return 0
        fi
@@ -97,9 +103,9 @@ function is_keystone_up() {
 }
 
 function get_tenant() {
-    id=$(keystone tenant-list | grep ' '$1' ' | awk '{print $2;}')
+    id=$(keystone $INSECURE_FLAG tenant-list | grep ' '$1' ' | awk '{print $2;}')
     if [ -z "$id" ]; then
-	id=$(keystone tenant-create --name=$1 | grep ' id ' | awk '{print $4}')
+	id=$(keystone $INSECURE_FLAG tenant-create --name=$1 | grep ' id ' | awk '{print $4}')
     fi
     echo $id
 }
@@ -118,10 +124,10 @@ INVIS_TENANT=$(get_tenant invisible_to_admin)
 
 # Users
 function get_user() {
-    id=$(keystone user-list | grep $1 | awk '{print $2;}')
+    id=$(keystone $INSECURE_FLAG user-list | grep $1 | awk '{print $2;}')
     EMAIL="@example.com"
     if [ -z $id ]; then
-	id=$(keystone user-create --name=$1 --pass="$ADMIN_PASSWORD" \
+	id=$(keystone $INSECURE_FLAG user-create --name=$1 --pass="$ADMIN_PASSWORD" \
 	         --email=$1$EMAIL | grep ' id ' | awk '{print $4;}')
     fi
     echo $id
@@ -133,9 +139,9 @@ DEMO_USER=$(get_user demo)
 # Roles
 
 function get_role() {
-    id=$(keystone role-list | grep ' '$1' ' | awk '{print $2;}')
+    id=$(keystone $INSECURE_FLAG role-list | grep ' '$1' ' | awk '{print $2;}')
     if [ -z $id ]; then
-	id=$(keystone role-create --name=$1 | grep ' id ' | awk '{print $4;}')
+	id=$(keystone $INSECURE_FLAG role-create --name=$1 | grep ' id ' | awk '{print $4;}')
     fi
     echo $id
 }
@@ -148,58 +154,58 @@ SYSADMIN_ROLE=$(get_role sysadmin)
 NETADMIN_ROLE=$(get_role netadmin)
 
 function user_role_lookup() {
-    echo $(keystone user-role-list --user-id $1 --tenant-id $2 \
+    echo $(keystone $INSECURE_FLAG user-role-list --user-id $1 --tenant-id $2 \
 	| grep ' '$3' ' | awk '{print $4;}')
 }
 
 # Add Roles to Users in Tenants
 if [ -z $(user_role_lookup $ADMIN_USER $ADMIN_TENANT admin) ]; then
-keystone user-role-add --user-id $ADMIN_USER --role-id $ADMIN_ROLE --tenant-id $ADMIN_TENANT
+keystone $INSECURE_FLAG user-role-add --user-id $ADMIN_USER --role-id $ADMIN_ROLE --tenant-id $ADMIN_TENANT
 fi
 
 if [ -z $(user_role_lookup $DEMO_USER $DEMO_TENANT Member) ]; then
-keystone user-role-add --user-id $DEMO_USER --role-id $MEMBER_ROLE --tenant-id $DEMO_TENANT
+keystone $INSECURE_FLAG user-role-add --user-id $DEMO_USER --role-id $MEMBER_ROLE --tenant-id $DEMO_TENANT
 fi
 
 if [ -z $(user_role_lookup $DEMO_USER $DEMO_TENANT sysadmin) ]; then
-keystone user-role-add --user-id $DEMO_USER --role-id $SYSADMIN_ROLE --tenant-id $DEMO_TENANT
+keystone $INSECURE_FLAG user-role-add --user-id $DEMO_USER --role-id $SYSADMIN_ROLE --tenant-id $DEMO_TENANT
 fi
 
 if [ -z $(user_role_lookup $DEMO_USER $DEMO_TENANT netadmin) ]; then
-keystone user-role-add --user-id $DEMO_USER --role-id $NETADMIN_ROLE --tenant-id $DEMO_TENANT
+keystone $INSECURE_FLAG user-role-add --user-id $DEMO_USER --role-id $NETADMIN_ROLE --tenant-id $DEMO_TENANT
 fi
 
 if [ -z $(user_role_lookup $DEMO_USER $INVIS_TENANT Member) ]; then
-keystone user-role-add --user-id $DEMO_USER --role-id $MEMBER_ROLE --tenant-id $INVIS_TENANT
+keystone $INSECURE_FLAG user-role-add --user-id $DEMO_USER --role-id $MEMBER_ROLE --tenant-id $INVIS_TENANT
 fi
 
 if [ -z $(user_role_lookup $ADMIN_USER $DEMO_TENANT admin) ]; then
-keystone user-role-add --user-id $ADMIN_USER --role-id $ADMIN_ROLE --tenant-id $DEMO_TENANT
+keystone $INSECURE_FLAG user-role-add --user-id $ADMIN_USER --role-id $ADMIN_ROLE --tenant-id $DEMO_TENANT
 fi
 
 # TODO(termie): these two might be dubious
 if [ -z $(user_role_lookup $ADMIN_USER $ADMIN_TENANT KeystoneAdmin) ]; then
-keystone user-role-add --user-id $ADMIN_USER --role-id $KEYSTONEADMIN_ROLE --tenant-id $ADMIN_TENANT
+keystone $INSECURE_FLAG user-role-add --user-id $ADMIN_USER --role-id $KEYSTONEADMIN_ROLE --tenant-id $ADMIN_TENANT
 fi
 if [ -z $(user_role_lookup $ADMIN_USER $ADMIN_TENANT KeystoneServiceAdmin) ]; then
-keystone user-role-add --user-id $ADMIN_USER --role-id $KEYSTONESERVICE_ROLE --tenant-id $ADMIN_TENANT
+keystone $INSECURE_FLAG user-role-add --user-id $ADMIN_USER --role-id $KEYSTONESERVICE_ROLE --tenant-id $ADMIN_TENANT
 fi
 
 # Services
 function get_service() {
-    id=$(keystone service-list | grep ' '$1' ' | awk '{print $2;}')
+    id=$(keystone $INSECURE_FLAG service-list | grep ' '$1' ' | awk '{print $2;}')
     if [ -z $id ]; then
-	id=$(get_id keystone service-create --name=$1 \
+	id=$(get_id keystone $INSECURE_FLAG service-create --name=$1 \
                         --type=$2 --description=$2)
     fi
     echo $id
 }
 
 function get_service_user() {
-    id=$(keystone user-list | grep $1 | awk '{print $2;}')
+    id=$(keystone $INSECURE_FLAG user-list | grep $1 | awk '{print $2;}')
     EMAIL="@example.com"
     if [ -z $id ]; then
-	id=$(keystone user-create --name=$1 --pass="$SERVICE_PASSWORD" \
+	id=$(keystone $INSECURE_FLAG user-create --name=$1 --pass="$SERVICE_PASSWORD" \
 	         --tenant-id $SERVICE_TENANT \
 	         --email=$1$EMAIL | grep ' id ' | awk '{print $4;}')
     fi
@@ -207,14 +213,14 @@ function get_service_user() {
 }
 
 function endpoint_lookup() {
-    echo $(keystone endpoint-list | grep ' '$1' ' | awk '{print $2;}' )
+    echo $(keystone $INSECURE_FLAG endpoint-list | grep ' '$1' ' | awk '{print $2;}' )
 }
 
 NOVA_SERVICE=$(get_service nova compute "Nova Compute Service")
 NOVA_USER=$(get_service_user nova)
 
 if [ -z $(user_role_lookup $NOVA_USER $SERVICE_TENANT admin) ]; then
-keystone user-role-add --tenant-id $SERVICE_TENANT \
+keystone $INSECURE_FLAG user-role-add --tenant-id $SERVICE_TENANT \
                        --user-id $NOVA_USER \
                        --role-id $ADMIN_ROLE
 fi
@@ -255,7 +261,7 @@ if [[ -n "$ENABLE_ENDPOINTS" ]]; then
                     --internalurl 'http://'$CONTROLLER':8774/v1.1/$(tenant_id)s'
             fi
         else
-            keystone endpoint-create --region $OS_REGION_NAME --service-id $NOVA_SERVICE \
+            keystone $INSECURE_FLAG endpoint-create --region $OS_REGION_NAME --service-id $NOVA_SERVICE \
                 --publicurl 'http://'$CONTROLLER':8774/v1.1/$(tenant_id)s' \
                 --adminurl 'http://localhost:$(compute_port)s/v1.1/$(tenant_id)s' \
                 --internalurl 'http://'$CONTROLLER':8774/v1.1/$(tenant_id)s'
@@ -266,7 +272,7 @@ fi
 EC2_SERVICE=$(get_service ec2 ec2 "EC2 Compatibility Layer")
 if [[ -n "$ENABLE_ENDPOINTS" ]]; then
     if [ -z $(endpoint_lookup $EC2_SERVICE) ]; then
-    keystone endpoint-create --region $OS_REGION_NAME --service-id $EC2_SERVICE \
+    keystone $INSECURE_FLAG endpoint-create --region $OS_REGION_NAME --service-id $EC2_SERVICE \
         --publicurl http://localhost:8773/services/Cloud \
         --adminurl http://localhost:8773/services/Admin \
         --internalurl http://localhost:8773/services/Cloud
@@ -277,7 +283,7 @@ GLANCE_SERVICE=$(get_service glance image "Glance Image Service")
 GLANCE_USER=$(get_service_user glance)
 
 if [ -z $(user_role_lookup $GLANCE_USER $SERVICE_TENANT admin) ]; then
-keystone user-role-add --tenant-id $SERVICE_TENANT \
+keystone $INSECURE_FLAG user-role-add --tenant-id $SERVICE_TENANT \
                        --user-id $GLANCE_USER \
                        --role-id $ADMIN_ROLE
 fi
@@ -285,12 +291,12 @@ fi
 if [[ -n "$ENABLE_ENDPOINTS" ]]; then
     if [ -z $(endpoint_lookup $GLANCE_SERVICE) ]; then
         if [ $ubuntu_mitaka -eq 1 ]; then
-            keystone endpoint-create --region $OS_REGION_NAME --service-id $GLANCE_SERVICE \
+            keystone $INSECURE_FLAG endpoint-create --region $OS_REGION_NAME --service-id $GLANCE_SERVICE \
                --publicurl http://$CONTROLLER:9292 \
                --adminurl http://localhost:9393 \
                --internalurl http://localhost:9393
         else
-           keystone endpoint-create --region $OS_REGION_NAME --service-id $GLANCE_SERVICE \
+           keystone $INSECURE_FLAG endpoint-create --region $OS_REGION_NAME --service-id $GLANCE_SERVICE \
               --publicurl http://$CONTROLLER:9292/v1 \
               --adminurl http://localhost:9393/v1 \
               --internalurl http://localhost:9393/v1
@@ -303,14 +309,14 @@ if [ $ubuntu_liberty_and_above -eq 1 ]; then
     BARBICAN_USER=$(get_service_user barbican)
 
     if [ -z $(user_role_lookup $BARBICAN_USER $SERVICE_TENANT admin) ]; then
-    keystone user-role-add --tenant-id $SERVICE_TENANT \
+    keystone $INSECURE_FLAG user-role-add --tenant-id $SERVICE_TENANT \
                            --user-id $BARBICAN_USER \
                            --role-id $ADMIN_ROLE
     fi
 
     if [[ -n "$ENABLE_ENDPOINTS" ]]; then
         if [ -z $(endpoint_lookup $BARBICAN_SERVICE) ]; then
-        keystone endpoint-create --region $OS_REGION_NAME --service-id $BARBICAN_SERVICE \
+        keystone $INSECURE_FLAG endpoint-create --region $OS_REGION_NAME --service-id $BARBICAN_SERVICE \
             --publicurl   http://$CONTROLLER:9311 \
             --adminurl    http://$CONTROLLER:9311 \
             --internalurl http://$CONTROLLER:9311
@@ -321,10 +327,10 @@ fi
 KEYSTONE_SERVICE=$(get_service keystone identity "Keystone Identity Service")
 if [[ -n "$ENABLE_ENDPOINTS" ]]; then
     if [ -z $(endpoint_lookup $KEYSTONE_SERVICE) ]; then
-    keystone endpoint-create --region $OS_REGION_NAME --service-id $KEYSTONE_SERVICE \
-        --publicurl 'http://'$CONTROLLER':5000/v2.0' \
-        --adminurl 'http://'$CONTROLLER':35357/v2.0' \
-        --internalurl 'http://'$CONTROLLER':35357/v2.0'
+    keystone $INSECURE_FLAG endpoint-create --region $OS_REGION_NAME --service-id $KEYSTONE_SERVICE \
+        --publicurl $AUTH_PROTOCOL'://'$CONTROLLER':5000/v2.0' \
+        --adminurl $AUTH_PROTOCOL'://'$CONTROLLER':35357/v2.0' \
+        --internalurl $AUTH_PROTOCOL'://'$CONTROLLER':35357/v2.0'
     fi
 fi
 
@@ -357,14 +363,14 @@ if [[ -z "$CINDER_USER" ]]; then
 fi
 
 if [ -z $(user_role_lookup $CINDER_USER $SERVICE_TENANT admin) ]; then
-keystone user-role-add --tenant-id $SERVICE_TENANT \
+keystone $INSECURE_FLAG user-role-add --tenant-id $SERVICE_TENANT \
                        --user-id $CINDER_USER \
                        --role-id $ADMIN_ROLE
 fi
 
 if [[ -n "$ENABLE_ENDPOINTS" ]]; then
     if [ -z $(endpoint_lookup $CINDER_SERVICE) ]; then
-    keystone endpoint-create --region $OS_REGION_NAME --service-id $CINDER_SERVICE \
+    keystone $INSECURE_FLAG endpoint-create --region $OS_REGION_NAME --service-id $CINDER_SERVICE \
         --publicurl 'http://'$CONTROLLER':8776/'$CINDER_SERVICE_TYPE'/$(tenant_id)s' \
         --adminurl 'http://localhost:9776/'$CINDER_SERVICE_TYPE'/$(tenant_id)s' \
         --internalurl 'http://localhost:9776/'$CINDER_SERVICE_TYPE'/$(tenant_id)s'
@@ -375,18 +381,18 @@ HORIZON_SERVICE=$(get_service "horizon" dashboard "OpenStack Dashboard")
 
 if [[ -n "$ENABLE_SWIFT" ]]; then
     SWIFT_SERVICE=$(get_id \
-    keystone service-create --name=swift \
+    keystone $INSECURE_FLAG service-create --name=swift \
                             --type="object-store" \
                             --description="Swift Service")
-    SWIFT_USER=$(get_id keystone user-create --name=swift \
+    SWIFT_USER=$(get_id keystone $INSECURE_FLAG user-create --name=swift \
                                              --pass="$SERVICE_PASSWORD" \
                                              --tenant-id $SERVICE_TENANT \
                                              --email=swift@example.com)
-    keystone user-role-add --tenant-id $SERVICE_TENANT \
+    keystone $INSECURE_FLAG user-role-add --tenant-id $SERVICE_TENANT \
                            --user-id $SWIFT_USER \
                            --role-id $ADMIN_ROLE
     if [[ -n "$ENABLE_ENDPOINTS" ]]; then
-        keystone endpoint-create --region $OS_REGION_NAME --service-id $SWIFT_SERVICE \
+        keystone $INSECURE_FLAG endpoint-create --region $OS_REGION_NAME --service-id $SWIFT_SERVICE \
             --publicurl   'http://localhost:8080/v1/AUTH_$(tenant_id)s' \
             --adminurl    'http://localhost:8080/v1/AUTH_$(tenant_id)s' \
             --internalurl 'http://localhost:8080/v1/AUTH_$(tenant_id)s'
@@ -397,14 +403,14 @@ if [[ -n "$ENABLE_QUANTUM" ]]; then
     QUANTUM_SERVICE=$(get_service quantum network "Quantum Service")
     QUANTUM_USER=$(get_service_user quantum)
     if [ -z $(user_role_lookup $QUANTUM_USER $SERVICE_TENANT admin) ]; then
-    keystone user-role-add --tenant-id $SERVICE_TENANT \
+    keystone $INSECURE_FLAG user-role-add --tenant-id $SERVICE_TENANT \
                            --user-id $QUANTUM_USER \
                            --role-id $ADMIN_ROLE
     fi
 
     if [[ -n "$ENABLE_ENDPOINTS" ]]; then
 	if [ -z $(endpoint_lookup $QUANTUM_SERVICE) ]; then
-        keystone endpoint-create --region $OS_REGION_NAME --service-id $QUANTUM_SERVICE \
+        keystone $INSECURE_FLAG endpoint-create --region $OS_REGION_NAME --service-id $QUANTUM_SERVICE \
             --publicurl http://'$CONTROLLER':9696 \
             --adminurl http://'$CONTROLLER':9696 \
             --internalurl http://'$CONTROLLER':9696
@@ -419,23 +425,23 @@ if [[ -n "$ENABLE_HEAT" ]]; then
     HEAT_CFN_SERVICE=$(get_service heat-cfn cloudformation "Orchestration Service")
     HEAT_USER=$(get_service_user heat)
     if [ -z $(user_role_lookup $HEAT_USER $SERVICE_TENANT admin) ]; then
-    keystone user-role-add --tenant-id $SERVICE_TENANT \
+    keystone $INSECURE_FLAG user-role-add --tenant-id $SERVICE_TENANT \
                            --user-id $HEAT_USER \
                            --role-id $ADMIN_ROLE
     fi
 
     if [[ -n "$ENABLE_ENDPOINTS" ]]; then
         if [ -z $(endpoint_lookup $HEAT_SERVICE) ]; then
-        keystone endpoint-create --region $OS_REGION_NAME --service-id $HEAT_SERVICE \
-            --publicurl 'http://'$CONTROLLER':8004/v1/%(tenant_id)s' \
-            --adminurl 'http://'$CONTROLLER:'8004/v1/%(tenant_id)s' \
-            --internalurl 'http://'$CONTROLLER':8004/v1/%(tenant_id)s'
+            keystone $INSECURE_FLAG endpoint-create --region $OS_REGION_NAME --service-id $HEAT_SERVICE \
+                --publicurl 'http://'$CONTROLLER':8004/v1/%(tenant_id)s' \
+                --adminurl 'http://'$CONTROLLER:'8004/v1/%(tenant_id)s' \
+                --internalurl 'http://'$CONTROLLER':8004/v1/%(tenant_id)s'
         fi
         if [ -z $(endpoint_lookup $HEAT_CFN_SERVICE) ]; then
-        keystone endpoint-create --region $OS_REGION_NAME --service-id $HEAT_CFN_SERVICE \
-            --publicurl 'http://'$CONTROLLER':8000/v1' \
-            --adminurl 'http://'$CONTROLLER:'8000/v1' \
-            --internalurl 'http://'$CONTROLLER':8000/v1'
+            keystone $INSECURE_FLAG endpoint-create --region $OS_REGION_NAME --service-id $HEAT_CFN_SERVICE \
+                --publicurl 'http://'$CONTROLLER':8000/v1' \
+                --adminurl 'http://'$CONTROLLER:'8000/v1' \
+                --internalurl 'http://'$CONTROLLER':8000/v1'
         fi
     fi
 fi
@@ -446,11 +452,11 @@ EC2RC=${EC2RC:-/etc/contrail/ec2rc}
 
 
 # create ec2 creds and parse the secret and access key returned
-RESULT=$(keystone ec2-credentials-create --tenant-id=$ADMIN_TENANT --user-id=$ADMIN_USER)
+RESULT=$(keystone $INSECURE_FLAG ec2-credentials-create --tenant-id=$ADMIN_TENANT --user-id=$ADMIN_USER)
 ADMIN_ACCESS=`echo "$RESULT" | grep access | awk '{print $4}'`
 ADMIN_SECRET=`echo "$RESULT" | grep secret | awk '{print $4}'`
 
-RESULT=$(keystone ec2-credentials-create --tenant-id=$DEMO_TENANT --user-id=$DEMO_USER)
+RESULT=$(keystone $INSECURE_FLAG ec2-credentials-create --tenant-id=$DEMO_TENANT --user-id=$DEMO_USER)
 DEMO_ACCESS=`echo "$RESULT" | grep access | awk '{print $4}'`
 DEMO_SECRET=`echo "$RESULT" | grep secret | awk '{print $4}'`
 
