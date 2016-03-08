8c4eb8bb6d3b6c94c479df642460d4d2fa86e78f
Megh Bhatt <meghb@juniper.net>
2016-03-07 16:42:05 -0800

    Remove leading spaces from options in contrail-query-engine conf template
    Change-Id: I4e7ddefe9f72a0a501a6325596dcf7874f02c5fb
    Closes-Bug: #1554277
    (cherry picked from commit 2df7c0a88be26de02c9139da3e51822848eecb3b)

--- contrail_provisioning/collector/templates/contrail_query_engine_conf.py
+++ contrail_provisioning/collector/templates/contrail_query_engine_conf.py
@@ -8,27 +8,27 @@ template = string.Template("""#
 
 [DEFAULT]
 # analytics_data_ttl=48
-  cassandra_server_list=$__contrail_cassandra_server_list__
+cassandra_server_list=$__contrail_cassandra_server_list__
 # collectors=127.0.0.1:8086
-  hostip=$__contrail_host_ip__ # Resolved IP of `hostname`
+hostip=$__contrail_host_ip__ # Resolved IP of `hostname`
 # hostname= # Retrieved as `hostname`
-  http_server_port=$__contrail_http_server_port__
+http_server_port=$__contrail_http_server_port__
 # log_category=
 # log_disable=0
-  log_file=$__contrail_log_file__
+log_file=$__contrail_log_file__
 # log_files_count=10
 # log_file_size=1048576 # 1MB
-  log_level=SYS_NOTICE
-  log_local=1
+log_level=SYS_NOTICE
+log_local=1
 # test_mode=0
 
 [DISCOVERY]
 # port=5998
-  server=$__contrail_discovery_ip__
+server=$__contrail_discovery_ip__
 
 [REDIS]
-  port=$__contrail_redis_server_port__
-  server=$__contrail_redis_server__
-  $__contrail_redis_password__
+port=$__contrail_redis_server_port__
+server=$__contrail_redis_server__
+$__contrail_redis_password__
 
 """)
