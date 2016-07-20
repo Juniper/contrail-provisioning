673f7a10d44310aa8d0511f39215161c26eb48b5
Nagendra Chandran <npchandran@juniper.net>
2016-07-20 10:47:22 +0000

    systemd: systemd support on openstack-services in centos/rhel Partial-Bug: 1604769
    Change-Id: I1d1f6c009534ad32ffd07095dc50e81870a15598

---
+++ contrail_provisioning/common/scripts/contrail-lib.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env bash
+
+#
+# checks if the target node is ubuntu based system
+# Verdict: echo True if the target node is ubuntu based system 
+#
+function is_ubuntu () {
+    [ -f /etc/lsb-release ] && egrep -q 'DISTRIB_ID.*Ubuntu' /etc/lsb-release && echo True || echo False
+}
+
+#
+# Starts supervisor-openstack services but all its child
+# Arguments:
+#
+function listen_on_supervisor_openstack_port () {
+    # centos/rhel based target nodes runs systemd initsytem and
+    # and supervisor-openstack is no longer supported on them
+    if [ $(is_ubuntu) == "True" ]; then
+        # Listen at supervisor-openstack port
+        status=$(service supervisor-openstack status | grep -s -i running >/dev/null 2>&1  && echo "running" || echo "stopped")
+        if [ $status == 'stopped' ]; then
+            service supervisor-openstack start
+            sleep 5
+            if [ -e /tmp/supervisord_openstack.sock ]; then
+                supervisorctl -s unix:///tmp/supervisord_openstack.sock stop all
+            else
+                supervisorctl -s unix:///var/run/supervisord_openstack.sock stop all
+            fi
+        fi
+    fi
+}
+
+#
+# Update services for the given action with sytemctl or service
+# command based on its initsystem
+# Arguments:
+#     action - action to be applied to the service
+#              eg: enable, start, stop, restart...
+#     services - one or list of services to be acted on
+#
+# Eg:
+# update_services enable keystone
+#     systemd: will run "systemctl enable keystone"
+#     sysv:    will run "chkconfig keystone on"
+#
+function update_services () {
+    action=$1
+    for service_name in "${@:2}"; do
+        if [ $(is_ubuntu) == "False" ]; then
+            systemctl $1 "$service_name"
+        else
+            if [ "$action" == "enable" ]; then
+                chkconfig $service_name on
+            else
+                service $service_name $action
+            fi
+        fi
+        if [ $? != 0 ]; then
+            echo "ERROR: "$action" service ( $service_name ) failed!"
+            exit 1
+        fi
+    done
+}
+
+#
+# Check if version of given rpm package is higher than
+# the reference version
+# Arguments:
+#     package_name: Name of the installed rpm package
+#     reference_version: Reference version to be compared
+#                        Format: '<epoch-number> <version> <release>'
+# Returns:
+# 0 - if the version of rpm package higher or equal to reference version
+# 1 - if the version of rpm package is lower than the reference version
+#
+function is_installed_rpm_greater() {
+    package_name=$1
+    read ref_epoch ref_version ref_release <<< $2
+    rpm -q --qf '%{epochnum} %{V} %{R}\n' $package_name >> /dev/null
+    if [ $? != 0 ]; then
+        echo "ERROR: Seems $package_name is not installed"
+        return 2
+    fi
+    read epoch version release <<< $(rpm -q --qf '%{epochnum} %{V} %{R}\n' $package_name)
+    verdict=$(python -c "import sys,rpm; \
+        print rpm.labelCompare(('$epoch', '$version', '$release'), ('$ref_epoch', '$ref_version', '$ref_release'))")
+    if [[ $verdict -ge 0 ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+#
+# Function to retrieve mysql service name
+# Centos7/Rhel7 are using mariadb instead of mysqld
+# 
+function get_mysql_service_name () {
+    [ -e /usr/lib/systemd/system/mariadb.service ] && echo mariadb || echo mysqld
+}
+
+# print error message and exit
+function error_exit
+{   
+    echo "${PROGNAME}: ${1:-''} ${2:-'Unknown Error'}" 1>&2
+    exit ${3:-1}
+}
+
