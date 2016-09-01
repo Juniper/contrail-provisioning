843da286ad3aa16de4ad76e813544c8d62c2e9be
Anish Mehta <amehta00@gmail.com>
2016-09-01 11:04:54 -0700

    Setting kafka retention policy to 256MB or 24 hours. Closes-Bug: 1619006
    Change-Id: Id9fad25d01c4a3ce3171ce5aac2f036e6b56e3b1

--- contrail_provisioning/database/setup.py
+++ contrail_provisioning/database/setup.py
@@ -156,6 +156,16 @@ class DatabaseSetup(DatabaseCommon):
         self.replace_in_file(KAFKA_SERVER_PROPERTIES, '#advertised.host.name=<hostname routable by clients>',\
                 'advertised.host.name='+listen_ip)
 
+        #Set retention policy
+        self.replace_in_file(KAFKA_SERVER_PROPERTIES, '#log.retention.bytes=.*',
+                'log.retention.bytes=1073741824')
+        self.replace_in_file(KAFKA_SERVER_PROPERTIES, 'log.retention.bytes=.*',
+                'log.retention.bytes=268435456')
+        self.replace_in_file(KAFKA_SERVER_PROPERTIES, 'log.segment.bytes=.*',
+                'log.segment.bytes=268435456')
+        self.replace_in_file(KAFKA_SERVER_PROPERTIES, 'log.retention.hours=.*',
+                'log.retention.hours=24')
+
         # Set log compaction and topic delete options
         self.replace_in_file(KAFKA_SERVER_PROPERTIES, 'log.cleaner.enable=false','log.cleaner.enable=true')
         if not self.file_pattern_check(KAFKA_SERVER_PROPERTIES, 'log.cleanup.policy=compact'):
