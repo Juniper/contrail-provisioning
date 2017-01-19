5b79b06900c3eda9847e0d7caef7fcfa91da674d
Nipa Kumar <nipak@juniper.net>
2017-01-19 13:08:06 -0800

    Provisioning related changes for removal of discovery on control-node
    Removal of discovery server ip and port and addition of list
    of collectors for control-node.
    
    Change-Id: Ic868343a67443a7315b6cd8f8a5b3669be487b9f
    Partial-Bug:1636319

--- contrail_provisioning/control/setup.py
+++ contrail_provisioning/control/setup.py
@@ -24,11 +24,10 @@ class ControlSetup(ContrailSetup):
 
         self.global_defaults = {
             'cfgm_ip': '127.0.0.1',
-            'collector_ip': '127.0.0.1',
-            'discovery_ip': '127.0.0.1',
             'self_ip': '127.0.0.1',
             'use_certs': False,
             'puppet_server': None,
+            'collectors' : None,
         }
 
         self.parse_args(args_str)
@@ -36,19 +35,20 @@ class ControlSetup(ContrailSetup):
 
     def parse_args(self, args_str):
         '''
-        Eg. setup-vnc-control --cfgm_ip 10.1.5.11 --collector_ip 10.1.5.11
-                                        --discovery_ip 10.1.5.11 --self_ip 10.1.5.12
+        Eg. setup-vnc-control --cfgm_ip 10.1.5.11
+                              --self_ip 10.1.5.12
                 --use_certs --puppet_server a3s19.contrail.juniper.net
+                --collectors 10.1.5.11:8086 10.1.5.12:8086
         '''
         parser = self._parse_args(args_str)
 
         parser.add_argument("--cfgm_ip", help = "IP Address of the openstack controller")
-        parser.add_argument("--collector_ip", help = "IP Address of the VNC collector")
-        parser.add_argument("--discovery_ip", help = "IP Address of the VNC discovery server")
         parser.add_argument("--self_ip", help = "IP Address of the VNC control node")
         parser.add_argument("--use_certs", help = "Use certificates for authentication",
             action="store_true")
         parser.add_argument("--puppet_server", help = "FQDN of Puppet Master")
+        parser.add_argument('--collectors', help = "List of IP:port of the VNC collectors",
+                            nargs="+")
 
         self._args = parser.parse_args(self.remaining_argv)
 
@@ -65,19 +65,20 @@ class ControlSetup(ContrailSetup):
         certdir = '/var/lib/puppet/ssl' if self._args.puppet_server else '/etc/contrail/ssl'
         template_vals = {'__contrail_ifmap_usr__': '%s' %(self.control_ip),
                          '__contrail_ifmap_paswd__': '%s' %(self.control_ip),
-                         '__contrail_discovery_ip__': self._args.cfgm_ip,
                          '__contrail_hostname__': self.hostname,
                          '__contrail_host_ip__': self.control_ip,
                          '__contrail_cert_ops__': '%s' %(certdir) if self._args.use_certs else '',
+                         '__contrail_collectors__': '%s' %(' '.join(self._args.collectors)),
+                         '__contrail_ifmap_server_url__': '%s' %('https://127.0.0.1:8443'),
                         }
         self._template_substitute_write(contrail_control_conf.template,
                                         template_vals, self._temp_dir_name + '/contrail-control.conf')
         local("sudo mv %s/contrail-control.conf /etc/contrail/contrail-control.conf" %(self._temp_dir_name))
 
     def fixup_contrail_control_nodemgr(self):
-        template_vals = {'__contrail_discovery_ip__' : self._args.cfgm_ip,
-                         '__contrail_discovery_port__': '5998'
-                       }
+        template_vals = {
+                         '__contrail_collectors__': '%s' %(' '.join(self._args.collectors)),
+                        }
         self._template_substitute_write(contrail_control_nodemgr_template.template,
                                         template_vals, self._temp_dir_name + '/contrail-control-nodemgr.conf')
         local("sudo mv %s/contrail-control-nodemgr.conf /etc/contrail/contrail-control-nodemgr.conf" %(self._temp_dir_name))
@@ -85,10 +86,11 @@ class ControlSetup(ContrailSetup):
     def fixup_dns(self):
         dns_template_vals = {'__contrail_ifmap_usr__': '%s.dns' %(self.control_ip),
                          '__contrail_ifmap_paswd__': '%s.dns' %(self.control_ip),
-                         '__contrail_discovery_ip__': self._args.cfgm_ip,
                          '__contrail_hostname__': self.hostname,
                          '__contrail_host_ip__': self.control_ip,
                          '__contrail_cert_ops__': '%s' %(certdir) if self._args.use_certs else '',
+                         '__contrail_collectors__': '%s' %(' '.join(self._args.collectors)),
+                         '__contrail_ifmap_server_url__': '%s' %('https://127.0.0.1:8443'),
                         }
         self._template_substitute_write(dns_conf.template,
                                         dns_template_vals, self._temp_dir_name + '/contrail-dns.conf')
