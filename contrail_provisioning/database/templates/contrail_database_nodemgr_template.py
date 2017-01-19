4af4d7c055dd58e6aee559fce0dfcd034a457ed3
Nipa Kumar <nipak@juniper.net>
2017-01-19 13:08:06 -0800

    Provisioning related changes for removal of discovery
    Removal of discovery server ip and port and addition of list
    of collectors for control-node.
    
    Changes for control-node, config, database, collector, compute roles.
    
    Change-Id: Ic868343a67443a7315b6cd8f8a5b3669be487b9f
    Partial-Bug:1636319

--- contrail_provisioning/database/templates/contrail_database_nodemgr_template.py
+++ contrail_provisioning/database/templates/contrail_database_nodemgr_template.py
@@ -1,15 +1,11 @@
 import string
 
 template = string.Template("""
-[DEFAULT]
+[DEFAULTS]
 hostip=$__hostip__
 minimum_diskGB=$__minimum_diskGB__
 
-[DISCOVERY]
-server=$__contrail_discovery_ip__
-port=$__contrail_discovery_port__
-
 [COLLECTOR]
-#server_list=ip1:port1 ip2:port2
+server_list=$__contrail_collectors__
 """)
 
