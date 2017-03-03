65edcba122bb94b744a37bc55e5b65b5a0e21bc0
Kumar Harsh <hkumar@juniper.net>
2017-03-03 11:15:06 +0530

    From R3.0 socket files are moved to /var/run from /tmp Hence for upgrade to releases  3.0 or greater edit correct path in toragent init script .
    Change-Id: If255df7ee9c8e747c9878ee630fdb4ddfd0fa25a
    Closes-Bug: #1666218

--- contrail_provisioning/compute/upgrade.py
+++ contrail_provisioning/compute/upgrade.py
@@ -4,6 +4,7 @@
 #
 """Upgrade's Contrail Compute components."""
 
+import os
 from distutils.version import LooseVersion
 
 from setup import ComputeSetup
@@ -83,6 +84,12 @@ class ComputeUpgrade(ContrailUpgrade, ComputeSetup):
                    nova_conf_file)
             local("service %s start" % openstack_compute_service)
 
+    def fix_tor_agent_init_script(self):
+        if not os.path.isfile('/tmp/supervisord_vrouter.sock'):
+            for tor_file in os.listdir('/etc/init.d/'):
+                if 'contrail-tor-agent' in tor_file:
+                    local("sed -i -e 's/tmp/var\/run/' %s%s" % ('/etc/init.d/', tor_file))
+
     def upgrade(self):
         self.disable_apt_get_auto_start()
         self._upgrade()
@@ -98,6 +105,9 @@ class ComputeUpgrade(ContrailUpgrade, ComputeSetup):
         if (self._args.from_rel < LooseVersion('2.20') and
             self._args.to_rel >= LooseVersion('2.20')):
             self.compute_setup.fixup_contrail_vrouter_nodemgr()
+        if 'toragent' in self._args.roles:
+            if (self._args.to_rel >= LooseVersion('3.00')):
+                self.fix_tor_agent_init_script()
         self.enable_apt_get_auto_start()
 
 def main():
