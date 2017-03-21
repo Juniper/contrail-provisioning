3616ab4bd7763c42d80d0cf076221763561bdff1
Kumar Harsh <hkumar@juniper.net>
2017-03-21 15:29:27 +0530

    Correct the indentation of IFMAP section in contrail-dns.conf
    Change-Id: Ie76a452567ca724d7a7e02d4c8f9eb6239023312
    Closes-Bug: #1667629

--- contrail_provisioning/control/templates/dns_conf.py
+++ contrail_provisioning/control/templates/dns_conf.py
@@ -15,22 +15,22 @@ collectors=$__contrail_collectors__
 # named_log_file=/var/log/contrail/contrail-named.log   # named log file
 # rndc_config_file=contrail-rndc.conf                   # rndc config file
 # rndc_secret=secretkey                                 # rndc secret
-  hostip=$__contrail_host_ip__ # Resolved IP of `hostname`
-  hostname=$__contrail_hostname__ # Retrieved as `hostname`
+hostip=$__contrail_host_ip__ # Resolved IP of `hostname`
+hostname=$__contrail_hostname__ # Retrieved as `hostname`
 # http_server_port=8092
 # dns_server_port=53
 # log_category=
 # log_disable=0
-  log_file=/var/log/contrail/contrail-dns.log
+log_file=/var/log/contrail/contrail-dns.log
 # log_files_count=10
 # log_file_size=1048576 # 1MB
-  log_level=SYS_NOTICE
-  log_local=1
+log_level=SYS_NOTICE
+log_local=1
 # test_mode=0
 
 [IFMAP]
-  rabbitmq_server_list=$__contrail_rabbit_server_list__
-  rabbitmq_user=$__contrail_rabbitmq_user__
-  rabbitmq_password=$__contrail_rabbitmq_password__
-  config_db_server_list=$__config_db_server_cql_list__
+rabbitmq_server_list=$__contrail_rabbit_server_list__
+rabbitmq_user=$__contrail_rabbitmq_user__
+rabbitmq_password=$__contrail_rabbitmq_password__
+config_db_server_list=$__config_db_server_cql_list__
 """)
