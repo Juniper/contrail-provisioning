afb653ccdb15a63286d2d3b952d47638e028046f
Kumar Harsh <hkumar@juniper.net>
2017-03-24 14:17:50 +0530

    For sriov update the nova.conf with scheduler_default_filters during upgrade .
    Change-Id: Ife9eb239af00dc6d2e97bc06af6a4c9a7bae0132
    Closes-Bug:#1627203

--- contrail_provisioning/openstack/upgrade.py
+++ contrail_provisioning/openstack/upgrade.py
@@ -98,6 +98,15 @@ class OpenstackUpgrade(ContrailUpgrade, OpenstackSetup):
         local("openstack-config --set /etc/nova/nova.conf neutron project_domain_name Default")
         local("openstack-config --set /etc/nova/nova.conf neutron user_domain_name Default")
 
+    def fix_sriov_nova_config(self):
+        with settings(warn_only=True):
+            nova_conf_file = '/etc/nova/nova.conf'
+            if (self._args.from_rel >= LooseVersion('3.00')):
+                default_filter= ('RetryFilter, AvailabilityZoneFilter, RamFilter, DiskFilter, '
+                                 'ComputeFilter, ComputeCapabilitiesFilter, ImagePropertiesFilter, '
+                                 'ServerGroupAntiAffinityFilter, ServerGroupAffinityFilter, PciPassthroughFilter')
+            local("openstack-config --set %s DEFAULT scheduler_default_filters '%s'" % (nova_conf_file, default_filter))
+
     def upgrade(self):
         self.stop()
         self._upgrade()
@@ -118,6 +127,8 @@ class OpenstackUpgrade(ContrailUpgrade, OpenstackSetup):
         if ('v3' in self._args.keystone_version and
                 self._args.from_rel <= LooseVersion('3.1.2.0')):
             self.fix_nova_config_kv3_params()
+        if (self._args.sriov):
+            self.fix_sriov_nova_config()
         self.restart()
 
 
