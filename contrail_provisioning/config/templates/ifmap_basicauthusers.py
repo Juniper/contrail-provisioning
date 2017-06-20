55009a6133feb70c5f202af4c5f4d6166d541841
Ignatious Johnson Christopher <ijohnson@juniper.net>
2017-06-19 21:30:07 -0700

    Use the ifmap password passed as commandline arg
    instead of the regular hardcoded password.
    
    Change-Id: I96d7d162fa7b50ad5659f1ae1237fc2da2a16687
    Partial-Bug: 1698928
    (cherry picked from commit 298b650d6f45723e445cda600a9fe64eaee44515)
    (cherry picked from commit 4e145e3c1bb44787dede2e24bed4f452f833fe3f)

--- contrail_provisioning/config/templates/ifmap_basicauthusers.py
+++ contrail_provisioning/config/templates/ifmap_basicauthusers.py
@@ -1,33 +1,11 @@
 import string
 
 template = string.Template("""
-test:test
-test2:test2
-test3:test3
-api-server:api-server
-schema-transformer:schema-transformer
-svc-monitor:svc-monitor
-control-user:control-user-passwd
-control-node-1:control-node-1
-control-node-2:control-node-2
-control-node-3:control-node-3
-control-node-4:control-node-4
-control-node-5:control-node-5
-control-node-6:control-node-6
-control-node-7:control-node-7
-control-node-8:control-node-8
-control-node-9:control-node-9
-control-node-10:control-node-10
-dhcp:dhcp
-visual:visual
-sensor:sensor
+api-server:$__ifmap_password__
+schema-transformer:$__ifmap_password__
+svc-monitor:$__ifmap_password__
+visual:$__ifmap_password__
 
-# compliance testsuite users
-mapclient:mapclient
-helper:mapclient
-
-# This is a read-only MAPC
-reader:reader
 $__contrail_control_node_users__
 $__contrail_control_node_dns_users__
 
