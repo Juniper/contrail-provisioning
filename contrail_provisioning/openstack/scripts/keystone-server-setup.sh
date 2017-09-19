9222bf425f7730ba075214043affee5af04ce4a6
Ignatious Johnson Christopher <ijohnson@juniper.net>
2017-09-19 13:18:42 -0700

    Provisioning neutron and contrail keystone auth
    config files with correct keystone v3 auth parameters.
    
    Change-Id: Ib62aa4721ff8a751686faa098a7fc494445c8276
    Closes-Bug: 1716430

--- contrail_provisioning/openstack/scripts/keystone-server-setup.sh
+++ contrail_provisioning/openstack/scripts/keystone-server-setup.sh
@@ -120,6 +120,7 @@ export OS_AUTH_URL=${AUTH_PROTOCOL}://$controller_ip:5000/v3
 export OS_USER_DOMAIN_NAME="Default"
 export OS_PROJECT_DOMAIN_NAME="Default"
 export OS_DOMAIN_NAME=Default
+export OS_PROJECT_NAME=admin
 export OS_IDENTITY_API_VERSION="3"
 export OS_USERNAME=admin
 export OS_PASSWORD=$ADMIN_PASSWORD
