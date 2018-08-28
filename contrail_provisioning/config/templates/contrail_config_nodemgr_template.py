b550443f730f99e685bebc16081bf1bd13688ce1
Nagendra Maynattamai <npchandran@juniper.net>
2018-08-27 22:51:50 -0700

    Add hostip and minimum disk space in contrail-config-nodemgr config Closes-Bug: 1779609
    Fix for 3.1.5-87:Provisioning issues in our code when db
    is split (Config and Analytics)
    
    Change-Id: Ifbbc98e2214e01384e7b42792e728fa7166620d2

--- contrail_provisioning/config/templates/contrail_config_nodemgr_template.py
+++ contrail_provisioning/config/templates/contrail_config_nodemgr_template.py
@@ -1,6 +1,10 @@
 import string
 
 template = string.Template("""
+[DEFAULT]
+hostip=$__contrail_config_database_ip__
+minimum_diskGB=$__contrail_config_minimum_disk__
+
 [DISCOVERY]
 server=$__contrail_discovery_ip__
 port=$__contrail_discovery_port__
